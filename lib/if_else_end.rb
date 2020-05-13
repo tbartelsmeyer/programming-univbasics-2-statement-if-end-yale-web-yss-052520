d1b998cbb2f6eeaf366417fc8cb41f1ebb97d7d8
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-13 07:26:16 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,10 @@
 # Write your solution here
 curr_time = Time.now
 even_ind = curr_time.to_i % 2
-  ==
+if even_ind == 0 
+  puts "Even!"
+  elsif even_ind == 1
+    puts "Odd!"
+  else
+    puts "Error!"
+  
